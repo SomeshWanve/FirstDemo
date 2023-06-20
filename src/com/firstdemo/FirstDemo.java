08b27cb2942a26f6b734a82312dc888d986ebf5a
Shubham vanve <Shubham vanve@LAPTOP-LC7M2V5D>
2023-06-20 06:59:40 +0530

    committed by somesh

---
+++ src/com/firstdemo/FirstDemo.java
@@ -0,0 +1,7 @@
+package com.firstdemo;
+
+public class FirstDemo {
+   public static void main(String[] args) {
+	System.out.println("hii");
+}
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author Shubham vanve
+ *
+ */
+module FirtsDemo {
+}
